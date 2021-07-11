8cbfce364ce5d2af946a0003a5f644a4dde51d5c
Jorge Vicente <jorge.eduardo@vicenteh.com>
2021-07-11 14:19:32 -0500

    Create crear_table_persona.sql
    Script de creación de table persona en la base de datos postgresql. El tipo de dato SERIAL es un tipo de dato integer que permite que se creen datos secuenciales.

---
+++ crear_table_persona.sql
@@ -0,0 +1,10 @@
+CREATE TABLE persona
+(
+    idpersona SERIAL,
+    nombres VARCHAR(20),
+    apellidopaterno VARCHAR(20),
+    apellidomaterno VARCHAR(20),
+    dni VARCHAR(8),
+    celular VARCHAR(9),
+    correo VARCHAR(30)
+);
